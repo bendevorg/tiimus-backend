8324a2cc47a18561864a48cfa53ea85801d5d45a
lucasl2 <lucalazarini@hotmail.com>
2018-08-16 23:18:41 -0300

    Retrieve project API

--- server/core/routers/project.js
+++ server/core/routers/project.js
@@ -7,6 +7,6 @@ const controllers = retrieveControllers(path.basename(__filename).split('.')[0])
 
 //  Skill API
 router.post('/new', controllers.newProject);
-// router.get('/', controllers.retrieveSkills);
+router.get('/', controllers.retrieveProject);
 
 module.exports = router;
