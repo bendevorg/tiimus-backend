9fd9c1149d7f4d3dbc75a22266f570f693d7f0d2
GuiMac <guibasconti@gmail.com>
2018-09-05 17:06:25 -0300

    Change API docs folder name

---
+++ docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Buildev",  "version": "0.0.1",  "description": "Buildev API documentation",  "title": "Buildev doc",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-09-05T20:06:16.999Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
