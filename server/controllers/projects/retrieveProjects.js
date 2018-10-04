72630cede50ab2ec1fd10a6a4a710e2bf9c30554
Lucas Lazarini <lucalazarini@hotmail.com>
2018-10-03 22:08:04 -0300

    Adjust retrieve projects layout output

---
+++ server/controllers/projects/retrieveProjects.js
@@ -0,0 +1,58 @@
+/**
+ * @api {GET} /projects Retrieve projects
+ * @apiName Retrieve projects
+ * @apiGroup Projects
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess (200) {json[]} msg Projects list.
+ * @apiSuccess (200) {String} id Project id.
+ * @apiSuccess (200) {String} name Project name.
+ * @apiSuccess (200) {String} description Project description.
+ * @apiSuccessExample {json} Success-Response:
+    "msg": [
+        {
+          "id": "012a362a-4f32-496f-bf25-d785d4df42ed",
+          "name": "Project example",
+          "description": "Project description example"
+        }
+    ]
+ * @apiError (500) {String} msg Error message.
+ * @apiErrorExample {json} Error-Response:
+    { "msg": "Database connection error." }
+  *
+ */
+const logger = require('../../../tools/logger');
+const database = require('../../models/database');
+const constants = require('../../utils/constants');
+
+
+/**
+ * Retrieve all projects
+ *
+ * @param {string}
+ * @return {object} - Returns the project in a json format
+ * @throws {object} - Returns a msg that indicates a failure
+ *
+ */
+module.exports = (req, res) => {
+  database.projects
+    .findAll({
+      attributes: ['id', 'name', 'description'],
+      include: [
+        { model: database.users, attributes: ['id', 'name'], through: { attributes: ['role'] } },
+        { model: database.tags, attributes: ['id', 'name'], through: { attributes: [] } },
+        { model: database.skills, attributes: ['id', 'name'], through: { attributes: [] } }
+      ]
+    })
+    .then(projects => {
+      return res.status(200).json({
+        msg: projects
+      });
+    })
+    .catch(err => {
+      logger.error(err);
+      return res.status(500).json({
+        msg: constants.messages.error.UNEXPECTED_DB
+      });
+    });
+};
