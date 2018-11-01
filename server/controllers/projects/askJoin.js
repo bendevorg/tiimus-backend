7b3c11595b0124e6b3d5be7bf71cc4ad33e298a4
Lucas Lazarini <lucalazarini@hotmail.com>
2018-10-31 21:12:13 -0300

    Initial documentation

---
+++ server/controllers/projects/askJoin.js
@@ -0,0 +1,19 @@
+/**
+ * @api {POST} /projects/:projectId/askJoin Ask to join project
+ * @apiName Ask to join project
+ * @apiGroup Projects
+ * @apiVersion 1.0.0
+ *
+ * @apiHeader {String} projectId Id of the project to be retrieved.
+ * 
+ * 
+ * @apiSuccess (200) {json[]} msg Projects list.
+ * @apiSuccess (200) {String} id Project id.
+ * 
+ * @apiSuccessExample {json} Success-Response:
+
+ * @apiError (500) {String} msg Error message.
+ * @apiErrorExample {json} Error-Response:
+    
+  *
+ */
